d36eb2ef43b5e04ead535fc4ef57e774ed6e7b96
lucaasgevaerd <lucaasgevaerd@gmail.com>
2024-06-09 22:41:35 -0300

    PUT product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -33,13 +33,24 @@ public class ProductService {
     public ProductDTO insert(ProductDTO dto) {
 
         Product entity = new Product();
+        copyToDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+
+        Product entity = repository.getReferenceById(id);
+        copyToDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyToDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
-        entity = repository.save(entity);
-
-        return new ProductDTO(entity);
     }
 }
